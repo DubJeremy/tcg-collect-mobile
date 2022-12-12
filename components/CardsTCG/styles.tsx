71fecc136811ee7966490d62b0c59a8f0b8326c4
DubJeremy <jeremy.ah33@gmail.com>
2022-12-12 16:42:55 +0100

    (Home): fetch and display the list of series

---
+++ components/CardsTCG/styles.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    loader: {},
+    loaderTxt: {},
+
+    cards: {},
+});
+
+export default styles;
--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,12 +1,47 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { FlatList, Text, View } from "react-native";
+import axios from "axios";
+
+import Serie from "../Serie";
+
 import styles from "./styles";
 
+const baseUrl = "https://api.tcgdex.net/v2/fr";
+
 export default function Home() {
+    const [series, setSeries] = useState();
+
+    const fetchSeriesTitle = () => {
+        axios
+            .get(`${baseUrl}/series`)
+            .then((response) => {
+                response.data.reverse()
+                setSeries(response.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    useEffect(() => {
+        fetchSeriesTitle();
+    }, []);
+
     return (
         <View style={styles.home}>
-            <Text style={styles.title}>All Series</Text>
-            <View style={styles.series}></View>
+            <Text style={styles.title}>Series</Text>
+            <View style={styles.series}>
+                <FlatList
+                    numColumns={2}
+                    data={series}
+                    renderItem={({ item }) => (
+                        <View style={styles.card}>
+                            <Serie props={[item.id, item.name, item.logo]} />
+                        </View>
+                    )}
+                    keyExtractor={(item) => item.id}
+                />
+            </View>
         </View>
     );
 }
